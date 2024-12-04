b709a3b6cccf7ac1c929e4e1dc7070307aa48e71
hammaddii <hammadriaz900@gmail.com>
2024-12-05 00:55:18 +0400

    search stuff

--- vue.js
+++ vue.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function () {
             },
 
             searchClubs() {
-                this.fetchProducts(this.searchQuery);
+                this.fetchProducts(this.searchQuery); 
             },
 
             goHome() {
